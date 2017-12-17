95d25c68e9800fb294226c2c614ae9ad3b96033b
Ravi Teja Gudapati <tejainece@gmail.com>
2017-12-17 06:15:15 +0100

    Help and Version commands

--- bin/dscript.dart
+++ bin/dscript.dart
@@ -3,6 +3,10 @@ import 'package:dscript/dscript.dart';
 import 'dart:collection';
 
 main(List<String> arguments) async {
+  if (probeSubCommands(arguments)) {
+    exit(0);
+  }
+
   final options = new Args.parse(arguments);
 
   final DetectedDartSdk sdk = new DartSdk.detect();
@@ -62,4 +66,40 @@ main(List<String> arguments) async {
   await stderr.flush();
 
   exit(exitCode);
+}
+
+void printHelp() {
+  print('dscript: Executes standalone Dart shell scripts.');
+  print('');
+  print('Usage: dscript [-v] [-k] [script-filename] [arguments...]');
+  print('Example: -v calc.dart 20 + 5');
+  print('');
+  print('Options:');
+  print('-v: Verbose');
+  print('-k: Keep temporary project files');
+  print('');
+  print('');
+  print('Sub-commands:');
+  print('help: Prints help text');
+  print('version: Prints version');
+}
+
+bool probeSubCommands(List<String> args) {
+  if (args.length == 0) {
+    print('Version: 1.0.0');
+    print('');
+    printHelp();
+    return true;
+  }
+
+  switch (args[0]) {
+    case 'version':
+      print('1.0.0');
+      return true;
+    case 'help':
+      printHelp();
+      return true;
+  }
+
+  return false;
 }
